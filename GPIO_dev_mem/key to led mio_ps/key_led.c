a84517f452bbfd30e732a5486ff57f7c1c9e82f9
imangholizadeh <45817368+imangholizadeh@users.noreply.github.com>
2023-05-15 14:45:46 +0330

    Update key_led.c

--- GPIO_dev_mem/key to led mio_ps/key_led.c
+++ GPIO_dev_mem/key to led mio_ps/key_led.c
@@ -7,8 +7,8 @@
 #define page_size 1024
 
 
-#define DATA_1_RO            0xFF0A0064 // mio26 DATA_0
-#define DATA_1               0xFF0A0044 // mio
+#define DATA_1_RO            0xFF0A0064 // mio26 key input
+#define DATA_1               0xFF0A0044 // mio40 led output
 
 
 
@@ -23,9 +23,8 @@ int main() {
  void *ptr;
 
  system("devmem 0xFF0A0000 w 0x0000DF0D");  // unlock
-
- system("devmem 0xFF0A0204 w 0x00000000");  // set as input (DIRM_0 ) //key
- system("devmem 0xFF0A0244 w 0x00004000");  // set as output (DIRM_1 ) //led mio 40
+	
+ system("devmem 0xFF0A0244 w 0x00004000");  // set as output (DIRM_1 ) //led mio_40 &&&&&& set as input (DIRM_1 ) //key mio_26
 
  system("devmem 0xFF0A0248 w 0x00004000");  // enable (OEN_1 ) //led mio 40
 
@@ -35,12 +34,12 @@ int main() {
 		return -1;
 	}
 
-
+//page_offset_input === key
         page_addr_input = (DATA_1_RO & (~(page_size-1)));
 	page_offset_input = DATA_1_RO - page_addr_input;
 
 
-
+//page_offset_output === LED
         page_addr = (DATA_1 & (~(page_size-1)));
 	page_offset = DATA_1 - page_addr;
 
